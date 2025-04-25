e70347b38623869c97e148149da46b48b588c1c7
Lukas Vedral <lukas.vedral.023@pslib.cz>
2025-04-25 11:30:43 +0200

    percentview

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import ViewBox from "./components/PercentView";
 
-export const  App = () => (
-    <p>Hello World</p>
-);
+
+
+
+export const App = () => {
+    let num: number = 34;
+    return(
+        <>
+        <ViewBox value={num} max={100} makeColor={(p) => "red"} />
+        <ViewBox value={num} max={200} makeColor={(p) => "green"} />
+        <ViewBox value={num} max={300} makeColor={(p) => "blue"} />
+        </>
+    )
+    
+};
